56822f4a7cc505e6e5031386c9a150b1003ad936
nicolebalbuena-gutierrez <nicole.balbuena@trincoll.edu>
2025-10-16 23:48:56 -0400

    Update LateFeeCalculator.java

--- src/main/java/edu/trincoll/service/fee/LateFeeCalculator.java
+++ src/main/java/edu/trincoll/service/fee/LateFeeCalculator.java
@@ -5,3 +5,11 @@ package edu.trincoll.service.fee;
  * Demonstrates Open-Closed Principle (OCP) - new fee structures can be added
  * without modifying existing code.
  */
+public interface LateFeeCalculator {
+    /**
+     * Calculate late fee based on days late
+     * @param daysLate Number of days the book is late
+     * @return The calculated late fee
+     */
+    double calculateLateFee(long daysLate);
+}
