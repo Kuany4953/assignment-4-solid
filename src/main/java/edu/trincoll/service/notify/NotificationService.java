ce2139d4e0de74075cd796239c32f142ed8e8cac
Kelly Asefa <kasefa@trincoll.edu>
2025-10-16 23:59:33 -0400

    Remove NotificationService interface

--- src/main/java/edu/trincoll/service/notify/NotificationService.java
+++ src/main/java/edu/trincoll/service/notify/NotificationService.java
@@ -1,10 +1,1 @@
-package edu.trincoll.service.notify;
 
-import edu.trincoll.model.Book;
-import edu.trincoll.model.Member;
-import java.time.LocalDate;
-
-public interface NotificationService {
-    void sendCheckoutNotification(Member member, Book book, LocalDate dueDate);
-    void sendReturnNotification(Member member, Book book, double lateFee);
-}
