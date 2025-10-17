14d77dd66550c380b6cb06d74bc0a35cdd30a4c2
nicolebalbuena-gutierrez <nicole.balbuena@trincoll.edu>
2025-10-16 23:52:56 -0400

    Update TypedLateFeeCalculator.java

--- src/main/java/edu/trincoll/service/fee/TypedLateFeeCalculator.java
+++ src/main/java/edu/trincoll/service/fee/TypedLateFeeCalculator.java
@@ -1,14 +1,1 @@
-package edu.trincoll.service.fee;
 
-import edu.trincoll.model.MembershipType;
-
-/**
- * TypedLateFeeCalculator extends LateFeeCalculator to add support for membership type identification.
- * This allows the factory to match calculators to specific membership types.
- */
-public interface TypedLateFeeCalculator extends LateFeeCalculator {
-    /**
-     * Return the membership type this calculator supports
-     */
-    MembershipType supports();
-}
