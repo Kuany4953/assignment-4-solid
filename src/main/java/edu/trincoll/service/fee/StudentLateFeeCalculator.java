78e3dfc29105395ed8eb338703b204d0e0afc506
nicolebalbuena-gutierrez <nicole.balbuena@trincoll.edu>
2025-10-16 23:52:42 -0400

    Update StudentLateFeeCalculator.java

--- src/main/java/edu/trincoll/service/fee/StudentLateFeeCalculator.java
+++ src/main/java/edu/trincoll/service/fee/StudentLateFeeCalculator.java
@@ -7,3 +7,15 @@ import org.springframework.stereotype.Component;
  * StudentLateFeeCalculator implements LateFeeCalculator for student members.
  * Student members are charged a reduced rate of $0.25 per day late.
  */
+@Component
+public class StudentLateFeeCalculator implements TypedLateFeeCalculator {
+    @Override
+    public MembershipType supports() {
+        return MembershipType.STUDENT;
+    }
+
+    @Override
+    public double calculateLateFee(long daysLate) {
+        return daysLate * 0.25;
+    }
+}
